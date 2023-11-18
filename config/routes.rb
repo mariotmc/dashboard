472d1f36709a1575892ec5850c9afe661b61f6c9
mariotmc <mariotomicx@gmail.com>
2023-11-18 20:50:39 +0100

    create task routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "dashboard#index"
 
   resources :dashboard, only: [:index]
+  namespace :dashboard do
+    resources :tasks
+  end
+
   resources :resources
   resources :reminders
 end
