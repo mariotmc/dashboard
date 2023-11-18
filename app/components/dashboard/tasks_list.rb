9e5a8f1f62d5f4f8e0eed9b313303eef79e1fecf
mariotmc <mariotomicx@gmail.com>
2023-11-19 00:09:24 +0100

    refactor task list attributes

--- app/components/dashboard/tasks_list.rb
+++ app/components/dashboard/tasks_list.rb
@@ -1,32 +1,14 @@
 module Dashboard
   class TasksList < ViewComponent::Base
-    def initialize(stage:)
-      @stage = stage
-      @title = stage.capitalize
-      set_variables(stage)
+    def initialize(tasks:, title:, color:, icon:, url:)
+      @tasks = tasks
+      @title = title
+      @color = color
+      @icon = icon
+      @url = url
     end
 
     private
       attr_reader :tasks, :title, :color, :icon, :url
-
-      def set_variables(stage)
-        case stage
-        when :active
-          @tasks ||= Task.active
-          @color ||= "emerald"
-          @icon ||= "circle-half-stroke"
-          @url ||= "new_dashboard_task_path?stage=active"
-        when :paused
-          @tasks ||= Task.paused
-          @color ||= "amber"
-          @icon ||= "circle-stop"
-          @url ||= "new_dashboard_task_path?stage=paused"
-        else
-          @tasks ||= Task.backlog
-          @color ||= "stone"
-          @icon ||= "circle-dot"
-          @url ||= "new_dashboard_task_path?stage=backlog"
-        end
-      end
   end
 end
