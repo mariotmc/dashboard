d8c3338449291bb5ae7a06f502e20c73d3988e28
mariotmc <mariotomicx@gmail.com>
2023-11-19 19:48:58 +0100

    destroy tasks if users gets deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   validates :email, :username, :password, presence: true
   validates :email, uniqueness: true
