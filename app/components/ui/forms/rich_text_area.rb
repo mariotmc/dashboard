fd396e19c95c511cbeed736ae5cf52ed6ba72cb6
mariotmc <mariotomicx@gmail.com>
2023-11-22 21:10:28 +0100

    add autofocus

--- app/components/ui/forms/rich_text_area.rb
+++ app/components/ui/forms/rich_text_area.rb
@@ -1,14 +1,15 @@
 module Ui
   module Forms
     class RichTextArea < ViewComponent::Base
-      def initialize(form:, field:, placeholder: nil)
+      def initialize(form:, field:, placeholder: nil, autofocus: false)
         @form = form
         @field = field
         @placeholder = placeholder
+        @autofocus = autofocus
       end
 
       private
-        attr_reader :form, :field, :placeholder
+        attr_reader :form, :field, :placeholder, :autofocus
     end
   end
 end
--- app/components/ui/forms/text_field.rb
+++ app/components/ui/forms/text_field.rb
@@ -1,14 +1,15 @@
 module Ui
   module Forms
     class TextField < ViewComponent::Base
-      def initialize(form:, field:, placeholder: nil)
+      def initialize(form:, field:, placeholder: nil, autofocus: false)
         @form = form
         @field = field
         @placeholder = placeholder
+        @autofocus = autofocus
       end
 
       private
-        attr_reader :form, :field, :placeholder
+        attr_reader :form, :field, :placeholder, :autofocus
     end
   end
 end
