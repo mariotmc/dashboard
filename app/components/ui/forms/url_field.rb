286b510f5b32449468352f98b34af245f952c2cf
mariotmc <mariotomicx@gmail.com>
2023-11-26 01:26:38 +0100

    fix new task form

--- app/components/ui/forms/url_field.rb
+++ app/components/ui/forms/url_field.rb
@@ -1,20 +1,19 @@
 module Ui
   module Forms
     class UrlField < ViewComponent::Base
-      def initialize(form:, field:, type:, optional: false, autofocus: false)
+      def initialize(form:, field:, optional: false, autofocus: false)
         @form = form
         @field = field
-        @type = type
         @optional = optional
         @autofocus = autofocus
         @collection = collection
       end
 
       private
-        attr_reader :form, :field, :type, :optional, :autofocus, :collection
+        attr_reader :form, :field, :optional, :autofocus, :collection
 
         def collection
-          case @type
+          case @field
           when :pull_request
            [["Github", "PullRequest::Github"]]
           when :external_task_tracker
--- app/controllers/dashboard/tasks_controller.rb
+++ app/controllers/dashboard/tasks_controller.rb
@@ -2,16 +2,27 @@ module Dashboard
   class TasksController < ApplicationController
     def new
       @task = Task.new
+      @task.pull_request = PullRequest.new
+      @task.external_task_tracker = ExternalTaskTracker.new
       @stage = params[:stage] || "backlog"
     end
 
     def create
       @task = Task.create(task_params)
+      @task.user = User.find_by(email: "jon@doe.com")
+
+      if @task.save
+        redirect_to dashboard_index_path
+      else
+        @stage = task_params[:stage]
+        render :new, status: :unprocessable_entity
+      end
     end
 
     private
       def task_params
-        params.require(:task).permit(:title, :notes, :priority, :stage, :pull_request, :external_task_tracker, :due_at)
+        params.require(:task).permit(:user, :stage, :title, :priority, :due_at, :notes,
+          external_task_tracker_attributes: [:type, :link], pull_request_attributes: [:type, :link])
       end
   end
 end
