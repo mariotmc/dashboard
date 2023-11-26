83fa9fe695f7d3bd8e2e3fb6374d7cf4b24721fe
mariotmc <mariotomicx@gmail.com>
2023-11-26 15:38:09 +0100

    create edit button component

---
+++ app/components/ui/buttons/edit.rb
@@ -0,0 +1,15 @@
+module Ui
+  module Buttons
+    class Edit < ViewComponent::Base
+      def initialize(url:, text: nil, icon: nil, css: nil)
+        @url = url
+        @text = text
+        @icon = icon
+        @css = css
+      end
+
+      private
+        attr_reader :url, :text, :icon, :css
+    end
+  end
+end
