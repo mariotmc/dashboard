0240e8f8f80180476e9b84b8cd5d2ad20b656aa1
mariotmc <mariotomicx@gmail.com>
2023-12-03 13:34:01 +0100

    add data attributes option

--- app/components/ui/buttons/action.rb
+++ app/components/ui/buttons/action.rb
@@ -1,17 +1,18 @@
 module Ui
   module Buttons
     class Action < Base
-      def initialize(text: nil, icon: nil, url:, method:, color:, css: nil)
+      def initialize(text: nil, icon: nil, url:, method:, data: {}, color:, css: nil)
         @text = text
         @icon = icon
         @url = url
         @method = method
+        @data = data
         @color = color
         @css = css
       end
 
       private
-        attr_reader :text, :icon, :url, :method, :color, :css
+        attr_reader :text, :icon, :url, :method, :data, :color, :css
     end
   end
 end
