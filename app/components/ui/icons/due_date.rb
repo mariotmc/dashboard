dfe60b7bcd124869daba222c9e10664e31b179de
mariotmc <mariotomicx@gmail.com>
2023-12-03 13:25:19 +0100

    add due_date css options

--- app/components/ui/icons/due_date.rb
+++ app/components/ui/icons/due_date.rb
@@ -1,12 +1,14 @@
 module Ui
   module Icons
     class DueDate < Base
-      def initialize(due_date:)
+      def initialize(due_date:, css: nil, icon_css: nil)
         @due_date = due_date
+        @css = css
+        @icon_css = icon_css
       end
 
       private
-        attr_reader :due_date
+        attr_reader :due_date, :css, :icon_css
     end
   end
 end
