36fd9c4b23d78dd8b65864b7edae9c840d7969de
mariotmc <mariotomicx@gmail.com>
2023-12-15 22:38:12 +0100

    drag and drop tasks between stages

--- app/controllers/tasks/positions_controller.rb
+++ app/controllers/tasks/positions_controller.rb
@@ -1,15 +1,20 @@
 module Tasks
   class PositionsController < ApplicationController
     before_action :set_task
+    before_action :set_stages
 
     def update
-      @task.update!(row_order_position: params[:row_order_position])
-      head :no_content
+      @task.update!(row_order_position: params[:row_order_position], stage_id: params[:destination_stage_id])
     end
 
     private
       def set_task
         @task = Task.find(params[:id])
+      end
+
+      def set_stages
+        @origin_stage = Stage.find(params[:origin_stage_id])
+        @destination_stage = Stage.find(params[:destination_stage_id])
       end
   end
 end
--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -3,15 +3,28 @@ import Sortable from "sortablejs";
 import { put } from "@rails/request.js";
 
 export default class extends Controller {
+  static values = { group: String };
+
   connect() {
     Sortable.create(this.element, {
       onEnd: this.onEnd.bind(this),
+      group: this.groupValue,
+      animation: 200,
     });
   }
 
   onEnd(event) {
     const url = event.item.dataset.sortableUrl;
+    const originStageId = event.from.dataset.sortableStageId;
+    const destinationStageId = event.to.dataset.sortableStageId;
 
-    put(url, { body: JSON.stringify({ row_order_position: event.newIndex }) });
+    put(url, {
+      body: JSON.stringify({
+        row_order_position: event.newIndex,
+        origin_stage_id: originStageId,
+        destination_stage_id: destinationStageId,
+      }),
+      responseKind: "turbo-stream",
+    });
   }
 }
