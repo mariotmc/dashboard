174c8d8899cf3070ce02f2e2179bcf4cc9106e11
mariotmc <mariotomicx@gmail.com>
2024-01-15 02:07:58 +0100

    create password component

---
+++ app/components/ui/forms/password_field.rb
@@ -0,0 +1,16 @@
+module Ui
+  module Forms
+    class PasswordField < Base
+      def initialize(form:, field:, placeholder: nil, optional: false, autofocus: false)
+        @form = form
+        @field = field
+        @placeholder = placeholder
+        @optional = optional
+        @autofocus = autofocus
+      end
+
+      private
+        attr_reader :form, :field, :placeholder, :optional, :autofocus
+    end
+  end
+end
