5c41f55159d7dd1d3a1d375e997cac01f10774df
mariotmc <mariotomicx@gmail.com>
2024-01-28 18:39:19 +0100

    fix task edit not in modal

--- app/components/ui/links/secondary.rb
+++ app/components/ui/links/secondary.rb
@@ -1,16 +1,17 @@
 module Ui
   module Links
     class Secondary < Base
-      def initialize(text: nil, url:, icon: nil, css: nil, icon_css: nil)
+      def initialize(text: nil, url:, icon: nil, turbo_frame: "_top", css: nil, icon_css: nil)
         @text = text
         @url = url
         @icon = icon
+        @turbo_frame = turbo_frame
         @css = css
         @icon_css = icon_css
       end
 
       private
-        attr_reader :text, :url, :icon, :css, :icon_css
+        attr_reader :text, :url, :icon, :turbo_frame, :css, :icon_css
     end
   end
 end
