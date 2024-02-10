c02bd236b12f1179fedf33596c3da1cc346554c5
mariotmc <mariotomicx@gmail.com>
2024-02-10 16:15:18 +0100

    add non turbo_stream option

--- app/components/ui/links/secondary.rb
+++ app/components/ui/links/secondary.rb
@@ -1,17 +1,18 @@
 module UI
   module Links
     class Secondary < Base
-      def initialize(text: nil, url:, icon: nil, turbo_frame: "_top", css: nil, icon_css: nil)
+      def initialize(text: nil, url:, icon: nil, turbo_stream: "", turbo_frame: "_top", css: nil, icon_css: nil)
         @text = text
         @url = url
         @icon = icon
+        @turbo_stream = turbo_stream
         @turbo_frame = turbo_frame
         @css = css
         @icon_css = icon_css
       end
 
       private
-        attr_reader :text, :url, :icon, :turbo_frame, :css, :icon_css
+        attr_reader :text, :url, :icon, :turbo_stream, :turbo_frame, :css, :icon_css
     end
   end
 end
