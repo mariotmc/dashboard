0c6f43598b75c24c30f96c6e33bdbb766c092d93
mariotmc <mariotomicx@gmail.com>
2024-03-30 01:43:54 +0100

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+stages = [{ status: 'active', row_order: 0 }, { status: 'paused', row_order: 1 }, { status: 'backlog', row_order: 2 }]
+
+stages.each do |stage|
+  Stage.find_or_create_by(status: stage[:status], row_order: stage[:row_order])
+end
