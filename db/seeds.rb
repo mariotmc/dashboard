b9ead2f8a09e324e6717dbd7ba60c29627f082d9
mariotmc <mariotomicx@gmail.com>
2024-04-09 17:51:33 +0200

    remove seeds

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-stages = [{ status: 'active', row_order: 0 }, { status: 'paused', row_order: 1 }, { status: 'backlog', row_order: 2 }]
-
-stages.each do |stage|
-  Stage.find_or_create_by(status: stage[:status], row_order: stage[:row_order])
-end
